2e8fe33a8f0ae816d2de0d922b238f5b7352786e
Can Kattwinkel <kattwinkel@w11k.de>
2018-07-30 11:13:47 +0200

    feat(lib): adds sticky thing directive

---
+++ projects/angular-sticky-things/src/lib/sticky-thing.directive.spec.ts
@@ -0,0 +1,8 @@
+import {StickyThingDirective} from './sticky-thing.directive';
+
+describe('StickyThingDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StickyThingDirective(null as any, null as any, null as any);
+    expect(directive).toBeTruthy();
+  });
+});
--- projects/angular-sticky-things/src/public_api.ts
+++ projects/angular-sticky-things/src/public_api.ts
@@ -2,3 +2,5 @@
  * Public API Surface of angular-sticky-things
  */
 
+export * from './lib/sticky-thing.directive';
+export * from './lib/angular-sticky-things.module';
