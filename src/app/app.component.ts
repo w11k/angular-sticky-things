57fe12a5ebf6e5ec3af97e0eeaf4e0226dac8243
Can Kattwinkel <kattwinkel@w11k.de>
2018-07-30 11:14:24 +0200

    feat(demo): adds demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'demo-root',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-sticky-things-demo';
+
+  constructor() {
+  }
+
 }
