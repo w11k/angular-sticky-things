cff6d4a626fbb44d9f781fa6b04dc559c8217a2a
Can Kattwinkel <kattwinkel@w11k.de>
2018-07-30 10:10:20 +0200

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('sticky-root h1')).getText();
+  }
+}
