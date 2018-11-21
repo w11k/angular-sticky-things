8fea926ac69c8041668cc1d07a223cc9cc1ba0e3
Alesandro Santoro <alessandro.santoro@paylogic.eu>
2018-11-21 16:35:09 +0100

    use button to enable sticky demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,16 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {interval, Subscription} from 'rxjs';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'demo-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
   enableSticky = true;
-
-  private interval: Subscription;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.interval = interval(10000).subscribe(() => this.enableSticky = !this.enableSticky);
-  }
-
-  ngOnDestroy() {
-    this.interval.unsubscribe();
   }
 }
