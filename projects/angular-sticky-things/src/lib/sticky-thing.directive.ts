89f8afd7c0b08611677cc2939601688f45a4b3d1
Alesandro Santoro <alessandro.santoro@paylogic.eu>
2018-11-23 14:12:21 +0100

    feat(sticky-directive): add marginTop input

--- projects/angular-sticky-things/src/lib/sticky-thing.directive.ts
+++ projects/angular-sticky-things/src/lib/sticky-thing.directive.ts
@@ -33,6 +33,7 @@ export interface StickyStatus {
 export class StickyThingDirective implements OnInit, AfterViewInit, OnDestroy {
 
 
+  @Input() marginTop = 0;
   @Input('spacer') spacerElement: HTMLElement | undefined;
   @Input('boundary') boundaryElement: HTMLElement | undefined;
 
@@ -130,7 +131,7 @@ export class StickyThingDirective implements OnInit, AfterViewInit, OnDestroy {
 
   private determineStatus(originalVals: StickyPositions, pageYOffset: number): StickyStatus {
     const stickyElementHeight = this.getComputedStyle(this.stickyElement.nativeElement).height;
-    const reachedLowerEdge = this.boundaryElement && window.pageYOffset + stickyElementHeight >= originalVals.bottomBoundary;
+    const reachedLowerEdge = this.boundaryElement && window.pageYOffset + stickyElementHeight >= (originalVals.bottomBoundary - this.marginTop);
     return {
       isSticky: pageYOffset > originalVals.offsetY,
       reachedLowerEdge
@@ -155,16 +156,15 @@ export class StickyThingDirective implements OnInit, AfterViewInit, OnDestroy {
       bottomBoundary = boundaryElementHeight + boundaryElementOffset;
     }
 
-    return {offsetY: getPosition(this.stickyElement.nativeElement).y, bottomBoundary};
+    return {offsetY: (getPosition(this.stickyElement.nativeElement).y - this.marginTop), bottomBoundary};
   }
 
   private makeSticky(boundaryReached: boolean = false): void {
 
     this.boundaryReached = boundaryReached;
-
     // do this before setting it to pos:fixed
     const {width, height, left} = this.getComputedStyle(this.stickyElement.nativeElement);
-    const offSet = boundaryReached ? (this.getComputedStyle(this.boundaryElement).bottom - this.getComputedStyle(this.stickyElement.nativeElement).height) : 0;
+    const offSet = boundaryReached ? (this.getComputedStyle(this.boundaryElement).bottom - height) : this.marginTop;
 
     this.sticky = true;
     this.stickyElement.nativeElement.style.position = 'fixed';
